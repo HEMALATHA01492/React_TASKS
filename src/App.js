e6972245f7c9f2cd33ccf02397e890f72a167a36
HEMALATHA01492 <hema01492@gmail.com>
2023-06-27 15:00:17 +0530

    Price Card

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-// import logo from './logo.svg';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import Home from './component/Home';
+import Card1 from './Card1';
+import Card2 from './Card2';
+import Card3 from './Card3';
 
-
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Home />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <h2 className="text-center p-3">Task 1 - Price cart bootstrap</h2>
+    <section className="pricing py-5">
+      <div className="container">
+       <div className="row">
+          <Card1 />
+          <Card2 />
+          <Card3 />
+       </div>
+     </div>
+    </section>
+  </div>
+  )
 }
+
 export default App;
