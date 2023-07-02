65c98afb9c53707fac95420d9af275657683f192
HEMALATHA01492 <hema01492@gmail.com>
2023-07-02 12:00:51 +0530

    star rating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import {Routes, Route} from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import Home1 from './pages/Home1';
-import Store from './pages/Store';
-import About from './pages/About';
-import NavBar from './component/NavBar';
-
-function App(){
-  return(
-    <>
-    <NavBar />
-    <Container>
-      <Routes>
-        <Route path ='/' element={<Home1/>}/>
-        <Route path ='/store' element={<Store />}/>
-        <Route path ='/about' element={<About />}/>
-
-      </Routes>
-    </Container>
-    </>
+import React, { useState } from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.css'
+function App() {
+  // const [hover,setHover]=useState('null')
+  const style={
+    color:'blue'
+  }
+  
+  return (
+    <div>
+      <span>
+      <i class="fa-solid fa-star checked" ></i>
+      <i class="fa-solid fa-star checked"></i>
+      <i class="fa-solid fa-star checked"></i>
+      <i class="fa-solid fa-star checked"></i>
+      <i class="fa-solid fa-star checked"></i>
+      </span>
+     
+    </div>
   )
 }
-export default App;
+
+export default App
+
+
+
+// import {Routes, Route} from 'react-router-dom';
+// import Home1 from './pages/Home1';
+// import Store from './pages/Store';
+// import About from './pages/About';
+// import NavBar from './component/NavBar';
+
+// function App(){
+//   return(
+//     <>
+//     <NavBar />
+//       <Routes>
+//         <Route path ='/' element={<Home1/>}/>
+//         <Route path ='/store' element={<Store />}/>
+//         <Route path ='/about' element={<About />}/>
+
+//       </Routes>
+//     </>
+//   )
+// }
+// export default App;
