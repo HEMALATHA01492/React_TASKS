5370bbcf9df7c16f05b29940c2ee53d4e27275f9
HEMALATHA01492 <hema01492@gmail.com>
2023-09-01 11:16:57 +0530

    CRUD

--- src/component/Create.js
+++ src/component/Create.js
@@ -1,25 +1,27 @@
-import axios from 'axios';
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Data from '../component/Data';
+
 
 function Create() {
-    const [value,setValue]=useState({
-        name:'',
-        age:'',
-        location:'',
-        contact:''
-    })
+    const [name,setName]=useState('');
+    const [age,setAge]=useState('');
+    const [location,setLocation]=useState('');
+    const [contact,setContact]=useState('');
+
+       
     const Navigate=useNavigate();
 
-    const handleSubmit=async(event)=>{
+    const handleSubmit=(event)=>{
         event.preventDefault();
-        await
-        axios.post('http://localhost:3005/users',value)
-        .then(response =>{
-          console.log(response);
-          Navigate('/')
-        })
-       .catch(err => console.log(err));
+        if(name==='' || age==='' ||location==='' || contact===''){
+           console.log("Enter All the Values")
+        }
+         else{
+            Data.push({id:Data.length+1,Name:name,Age:age,Location:location,Contact:contact})
+            Navigate('/')
+         }
+        
     }
   return (
     <div className='d-flex w-100 vh-100 justify-content-center align-items-center bg-light'>
@@ -29,29 +31,33 @@ function Create() {
               <div className="mb-3 row">
                      <label htmlFor="input" className="col-sm-2 col-form-label">Name</label>
                   <div className="col-sm-10">
-                     <input type="text" className="form-control" placeholder='Enter your name here'
-                     onChange={e=>setValue({...value,name:e.target.value})} />
+                     <input type="text" className="form-control" placeholder='Enter your name here'                    
+                      required
+                     onChange={e=>setName(e.target.value)} />
                   </div>
                 </div>
-              <div class="mb-3 row">
+              <div className="mb-3 row">
                      <label htmlFor="input" className="col-sm-2 col-form-label">Age</label>
-                  <div class="col-sm-10">
+                  <div className="col-sm-10">
                      <input type="text" className="form-control" placeholder='Enter your age here' 
-                      onChange={e=>setValue({...value,age:e.target.value})}/>
+                     required
+                      onChange={e=>setAge(e.target.value)}/>
                    </div>
                </div>
-               <div class="mb-3 row">
+               <div className="mb-3 row">
                      <label htmlFor="input" className="col-sm-2 col-form-label">Location</label>
-                  <div class="col-sm-10">
+                  <div className="col-sm-10">
                      <input type="text" className="form-control" placeholder='Enter your location here' 
-                      onChange={e=>setValue({...value,location:e.target.value})}/>
+                     required
+                     onChange={e=>setLocation(e.target.value)}/>
                    </div>
                </div>
-               <div class="mb-3 row">
+               <div className="mb-3 row">
                      <label htmlFor="input" className="col-sm-2 col-form-label">Contact</label>
-                  <div class="col-sm-10">
+                  <div className="col-sm-10">
                      <input type="text" className="form-control" placeholder='Enter your contact number here' 
-                     onChange={e=>setValue({...value,contact:e.target.value})}/>
+                      required
+                     onChange={e=>setContact(e.target.value)}/>
                    </div>
                </div>
                <button type="button" className="btn btn-success m-2" onClick={handleSubmit}>Submit</button>
--- src/component/Read.js
+++ src/component/Read.js
@@ -1,30 +1,25 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useParams,Link } from 'react-router-dom';
-
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import Data from '../component/Data';
 function Read() {
- const[data,setData]=useState([])
-  const {id}=useParams();
-  useEffect(()=>{
-    axios.get('http://localhost:3005/users/'+id)
-    .then(res => setData(res.data))
-    .catch(err => console.log(err));
-  },[])
-
+  
+  const id= useParams().id;
+  const out=Data.find(n => n.id === Number(id))
+ 
   return (
     <div>
         <div className='d-flex w-100 vh-100 justify-content-center align-items-center bg-light'>
         <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
-              <div className="">
-                     <div className="justify-content-center align-items-center"><h5>User id:{data.id}</h5></div>
-                     <div className="justify-content-center align-items-center"><h5>Name:{data.name}</h5></div>
-                     <div className="justify-content-center align-items-center"><h5>Age:{data.age}</h5></div>
-                     <div className="justify-content-center align-items-center"><h5>Location:{data.location}</h5></div>
-                     <div className="justify-content-center align-items-center"><h5>Contact:{data.contact}</h5></div>
+              <div>
+                <h2  className="text-center p-2">User Details</h2>
+                     <div className="justify-content-center align-items-center"><h5>User id:{out.id}</h5></div>
+                     <div className="justify-content-center align-items-center"><h5>Name:{out.Name}</h5></div>
+                     <div className="justify-content-center align-items-center"><h5>Age:{out.Age}</h5></div>
+                     <div className="justify-content-center align-items-center"><h5>Location:{out.Location}</h5></div>
+                     <div className="justify-content-center align-items-center"><h5>Contact:{out.Contact}</h5></div>
 
-                  
+                
                 </div>
-                <Link to={`/edit/${id}`} className="btn btn-success  justify-items-end m-2 mt-4">Edit</Link>
                 <Link to='/' className="btn btn-danger  justify-items-end m-2 mt-4">Close</Link>
         </div>
     </div>
